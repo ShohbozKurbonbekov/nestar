4c5f2a37f94366378e954ce9ed2fa8155a15951b
ShohbozKurbonbekov <skurbonbekov98@gmail.com>
2025-06-29 00:38:18 +0900

    feat: developing likeTargetMember GraphQl API

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -74,6 +74,16 @@ export class MemberResolver {
 		return await this.memberService.getMember(memberId, targetId);
 	}
 
+	@UseGuards(AuthGuard)
+	@Mutation(() => Member)
+	public async likeTargetMember(
+		@Args('memberId') input: string,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Member> {
+		console.log('Mutation likeTargetMember');
+		const likeRefId = shapeIntoMongoObjectId(input);
+		return await this.memberService.likeTargetMember(memberId, likeRefId);
+	}
 	/* ADMIN */
 	// Authorization: Admin
 	@Roles(MemberType.ADMIN)
